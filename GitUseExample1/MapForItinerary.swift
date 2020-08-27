2aba33913f199de6fc619078fb06fab7308bacda
David Juett <djuett501@gmail.com>
2020-08-27 13:56:38 -0400

    Add new MapForItinerary feature

---
+++ GitUseExample1/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample1
+//
+//  Created by  on 8/27/20.
+//  Copyright © 2020 David Juett. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
