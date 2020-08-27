9ed02d1af7b603befa5204846d646bd2b960701e
David Juett <djuett501@gmail.com>
2020-08-27 13:39:48 -0400

    Log Message on app launch Add button on UI that logs to console when pressed

--- GitUseExample1/AppDelegate.swift
+++ GitUseExample1/AppDelegate.swift
@@ -13,8 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication,
+            didFinishLaunchingWithOptions launchOptions:
+    [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("Application did finish launching")
         return true
     }
 
