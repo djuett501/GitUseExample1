c47801a73853f8018e23be7b263ef04d8aaa195e
David Juett <djuett501@gmail.com>
2020-08-27 13:14:05 -0400

    Initial Commit

---
+++ GitUseExample1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample1
+//
+//  Created by  on 8/27/20.
+//  Copyright © 2020 David Juett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
